6753dad22a9c07cf87d649b1033ece4181be83b3
Ghegi Jimenez <dev@gisketch.com>
2025-03-15 17:22:28 +0800

    remove door interaction

--- Assets/Scripts/Utilities/DoorQuit.cs
+++ Assets/Scripts/Utilities/DoorQuit.cs
@@ -8,28 +8,49 @@ public class DoorQuit : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     private bool isHovering = false;
 
-    void Update()
-    {
-
-        if (isHovering)
-        {
-            if (InputManager.Instance.GetPrimaryButton())
-            {
-                GameManager.Instance.ResetScene();
-            }
-        }
-
-    }
-
     public void OnPointerEnter(PointerEventData eventData)
     {
         isHovering = true;
-        GameManager.Instance.SetInteractionMessage("Exit");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         isHovering = false;
-        GameManager.Instance.ClearInteractionMessage();
+    }
+
+    void Update()
+    {
+        if (isHovering && InputManager.Instance.GetPrimaryButton())
+        {
+            // Properly shutdown network before scene reset
+            // DisconnectAndResetScene();
+            Application.Quit();
+        }
+    }
+
+    private void DisconnectAndResetScene()
+    {
+        // Check if we're connected to a network session
+        if (Mirror.NetworkClient.isConnected)
+        {
+            // Stop client if we're a client
+            Mirror.NetworkClient.Disconnect();
+        }
+
+        // Stop host if we're a host/server
+        if (Mirror.NetworkServer.active)
+        {
+            Mirror.NetworkManager.singleton.StopHost();
+        }
+
+        // Wait briefly for network to clean up, then reset scene
+        StartCoroutine(DelayedReset());
+    }
+
+    private IEnumerator DelayedReset()
+    {
+        // Wait for network to clean up
+        yield return new WaitForSeconds(0.5f);
+        GameManager.Instance.ResetScene();
     }
 }
